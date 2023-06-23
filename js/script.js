06f0cee3fc45abf9b955cfdbdf27e551401e868a
Edvaldo-J <105723554+Edvaldo-J@users.noreply.github.com>
2023-06-23 12:19:15 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function abrirhomem() {
+    alert('Função não habilitada!')
+}
+
+function abrirmulher() {
+    alert('Função não habilitada!')
+}
+
+function abrircrianca() {
+    alert('Função não habilitada!')
+}
